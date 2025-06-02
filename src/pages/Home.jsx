509a325a5560654f88c65260e101139b8d0ffcfc
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2025-06-02 15:22:42 +0400

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import softwgirlimg from '../assets/prog.png' 
+import softwgirlimg from '../assets/mainpic.png' 
 import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
     <div className='w-[80%] flex flex-col justify-center pl-[30px] md:pl-[50px] gap-[5px] md:gap-[60px]'>
         <div className='flex flex-col md:flex-row items-start md:items-center mt-[53px] md:mt-[100px] gap-[5px] md:gap-[100px]'>
-        <img src={softwgirlimg} alt="img" width={400} height={200} />
+        <img src={softwgirlimg} alt="img" width={300} height={200} />
         <div className='text-[40px] font-bold text-[#d74e09]'>
             <p>Hi,</p>
             <p>I'm Nazrin</p>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div className='pl-[30px] pt-[50px]'>
       <p className='mb-[10px] text-[#30190e] text-[18px]'>You can link to my CV file via this button.</p>
-      <a href="https://drive.google.com/file/d/1sr2wfzJ5A2ssuK_fiIT7A9yJ7zMtZqQz/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+      <a href="https://drive.google.com/file/d/1sr2wfzJ5A2ssuK_fiIT7A9yJ7zMtZqQz/view?usp=sharing" target="_blank" rel="noopener noreferrer">
       <button className='p-[10px] bg-[#d74e09] border-none rounded-[8px] cursor-pointer font-semibold text-[#30190e]'>
       Resume
       </button>
